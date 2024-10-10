8c61510d70cb1aecd66a7b2309c0297c41516d88
Josh Fried <joshuafried@gmail.com>
2024-10-10 03:19:07 -0400

    checkpoint

--- socialNetwork/README.md
+++ socialNetwork/README.md
@@ -79,6 +79,13 @@ And then go back to the socialNetwork folder to run the wrk2 properly for the sy
 cd ../socialNetwork
 ```
 
+../wrk2/wrk -D exp -t 10 -c 10 -d 10 -L -s ./wrk2/scripts/social-network/compose-post.lua http://172.32.0.30:8080/wrk2-api/post/compose -R 1000
+../wrk2/wrk -D exp -t 4 -c 4 -d 30 -L -s ./wrk2/scripts/social-network/compose-post.lua http://172.32.0.30:8080/wrk2-api/post/compose -R 100
+
+
+../wrk2/wrk -D exp -t 10 -c 10 -d 10 -L -s ./wrk2/scripts/social-network/read-home-timeline.lua http://172.32.0.30:8080/wrk2-api/home-timeline/read -R 1000
+../wrk2/wrk -D exp -t 10 -c 10 -d 10 -L -s ./wrk2/scripts/social-network/read-user-timeline.lua http://172.32.0.30:8080/wrk2-api/user-timeline/read -R 1000
+
 #### Compose posts
 
 ```bash
--- socialNetwork/run.sh
+++ socialNetwork/run.sh
@@ -16,7 +16,7 @@ write_config() {
 host_addr $ip
 host_netmask 255.255.0.0
 host_gateway 172.32.0.1
-runtime_kthreads 2
+runtime_kthreads 1
 runtime_spinning_kthreads 0
 runtime_guaranteed_kthreads 0
 runtime_priority lc
@@ -45,26 +45,26 @@ start_service() {
   ~/junction/build/junction/junction_run ${tmp}/${sname}.config --ld_path $LD_LIBRARY_PATH -- /users/friedj/install/${sname} > ${tmp}/${sname}.log 2>&1 &
 }
 
-# start_service PostStorageService 172.32.0.8
+start_service PostStorageService 172.32.0.8
 start_service HomeTimelineService 172.32.0.23
 start_service UserTimelineService 172.32.0.22
 start_service UserService 172.32.0.17
 start_service SocialGraphService 172.32.0.5
-# start_service ComposePostService 172.32.0.16
+start_service ComposePostService 172.32.0.16
 start_service UniqueIdService 172.32.0.2
 start_service MediaService 172.32.0.3
 start_service TextService 172.32.0.13
 start_service UrlShortenService 172.32.0.25
 start_service UserMentionService 172.32.0.20
 
-write_config 172.32.0.8 $tmp/post-storage-service.config
-~/junction/build/junction/junction_run $tmp/post-storage-service.config --ld_path $LD_LIBRARY_PATH -s -- /users/friedj/install/PostStorageService > $tmp/post-storage-service.log 2>&1 &
+# write_config 172.32.0.8 $tmp/post-storage-service.config
+# ~/junction/build/junction/junction_run $tmp/post-storage-service.config --ld_path $LD_LIBRARY_PATH -- /users/friedj/install/PostStorageService > $tmp/post-storage-service.log 2>&1 &
 
 # write_config 172.32.0.23 $tmp/home-timeline-service.config
 # ~/junction/build/junction/junction_run $tmp/home-timeline-service.config --ld_path $LD_LIBRARY_PATH -- /users/friedj/install/HomeTimelineService > $tmp/home-timeline-service.log 2>&1 &
 
-write_config 172.32.0.16 $tmp/compose-post-service.config
-~/junction/build/junction/junction_run $tmp/compose-post-service.config --ld_path $LD_LIBRARY_PATH -s -- /users/friedj/install/ComposePostService > $tmp/compose-post-service.log 2>&1 &
+# write_config 172.32.0.16 $tmp/compose-post-service.config
+# ~/junction/build/junction/junction_run $tmp/compose-post-service.config --ld_path $LD_LIBRARY_PATH -- /users/friedj/install/ComposePostService > $tmp/compose-post-service.log 2>&1 &
 
 # write_config 172.32.0.12 $tmp/post-storage-service.config
 # ~/junction/build/junction/junction_run $tmp/post-storage-service.config --ld_path $LD_LIBRARY_PATH -- /users/friedj/install/PostStorageService > $tmp/post-storage-service.log 2>&1 &
