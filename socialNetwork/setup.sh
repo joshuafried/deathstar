6b81ff6f82d7e306885966243c2a45497b2e274e
Josh Fried <joshuafried@gmail.com>
2024-10-14 08:15:00 -0600

    nits

--- socialNetwork/setup.sh
+++ socialNetwork/setup.sh
@@ -4,6 +4,8 @@
 set -e
 set -x
 
+sudo apt-get install -y libz-dev libssl-dev luarocks
+
 pushd docker/thrift-microservice-deps/cpp
 docker build -t thrift-microservice-deps:jammy .
 popd
@@ -12,8 +14,14 @@ pushd redis
 docker build -t socialnet_redis .
 popd
 
-
-
 docker build -t social-network-microservices:latest .
 
 sudo docker network create --subnet 172.32.0.0/16 socialnet || true
+
+cd ..
+
+git submodule update --init --recursive
+pushd wrk2
+make -j "$(nproc)"
+
+ sudo luarocks install luasocket
